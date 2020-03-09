ca28311c2dcbdf0e5378e4af4eef63a841126e87
myrtosh <61792816+myrtosh@users.noreply.github.com>
2020-03-09 11:59:36 +0100

    Add files via upload

--- httpservice.go
+++ httpservice.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,6 +9,11 @@ import (
 
 	"github.com/fatih/camelcase"
 )
+
+type Profile struct {
+	Git_hash            string
+	Name_of_the_project string
+}
 
 func world(w http.ResponseWriter, r *http.Request) {
 	value, _ := r.URL.Query()["name"]
@@ -18,13 +24,20 @@ func world(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello %v!", strings.Join(splitted, " "))
 	} else if r.URL.Path[1:] == "helloworld" {
 		fmt.Fprintln(w, "Hello Stranger!")
+	} else if r.URL.Path[1:] == "versionz" {
+		profile := Profile{"dd6492921d3fec85f9dcedfa58b20702b2e781a6", "httpservice"}
+
+		js, err := json.Marshal(profile)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
 	}
 }
-
 func main() {
 	http.HandleFunc("/", world)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
-// localhost:8080/helloworld?name=MyrtoSeisa
-// localhost:8080/helloworld
