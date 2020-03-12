3167e71dc609985dd47adc9bf7f9df49c5c18b6c
myrtosh <myrto-sherin@hotmail.com>
2020-03-12 14:53:12 +0100

    8080:/versionz returns status

--- httpservice.go
+++ httpservice.go
@@ -25,8 +25,10 @@ func world(w http.ResponseWriter, r *http.Request) {
 		splitted := camelcase.Split(value[0])
 
 		fmt.Fprintf(w, "Hello %v!", strings.Join(splitted, " "))
+		CheckHTTPStatus()
 	} else if r.URL.Path[1:] == "helloworld" {
 		fmt.Fprintln(w, "Hello Stranger!")
+		CheckHTTPStatus()
 	} else if r.URL.Path[1:] == "versionz" {
 
 		profile := Profile{GitCommit, "httpservice"}
@@ -39,6 +41,19 @@ func world(w http.ResponseWriter, r *http.Request) {
 
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(js)
+		CheckHTTPStatus()
+	}
+}
+
+func CheckHTTPStatus() {
+	resp, _ := http.Get("http://localhost:8080")
+
+	log.Println("HTTP Response Status:", resp.StatusCode, http.StatusText(resp.StatusCode))
+
+	if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+		log.Println("HTTP Status is in the 2xx range")
+	} else {
+		log.Println("Argh! Broken")
 	}
 }
 
