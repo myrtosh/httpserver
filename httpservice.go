5fd7ee8f390acd58c1222e28df0d37984933733a
myrtosh <myrto-sherin@hotmail.com>
2022-04-07 21:55:41 +0300

    added HTTP status and time

--- httpservice.go
+++ httpservice.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
 type Profile struct {
@@ -15,6 +17,7 @@ var GitCommit string
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Hello Stranger!")
+	CheckHTTPStatus()
 }
 
 func version(w http.ResponseWriter, req *http.Request) {
@@ -24,14 +27,29 @@ func version(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.Header().Set("Content Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
+	CheckHTTPStatus()
 }
+
+func CheckHTTPStatus() {
+	resp, _ := http.Get("http://localhost:8080")
+	log.Println("HTTP Response Status:", resp.StatusCode, http.StatusText(resp.StatusCode))
+	if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+		log.Println("HTTP Status is in the 2xx range")
+	} else {
+		log.Println("Argh! Broken")
+	}
+}
+
 func main() {
 
 	http.HandleFunc("/helloworld", hello)
 
 	http.HandleFunc("/versionz", version)
 
-	http.ListenAndServe(":8080", nil)
+	t := time.Now()
+	fmt.Println(t.Format("2006-01-02-15:04:05"))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
