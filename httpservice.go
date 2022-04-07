d12fb415dbd1719476095db6fbe337fa3d03e4e0
myrtosh <myrto-sherin@hotmail.com>
2022-04-07 20:55:42 +0300

    3rd endpoint versionz

--- httpservice.go
+++ httpservice.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type Profile struct {
+	GitHash          string
+	NameOfTheProject string
+}
+
+var GitCommit string
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Hello Stranger!")
 }
 
+func version(w http.ResponseWriter, req *http.Request) {
+	profile := Profile{GitCommit, "httpservice"}
+	js, err := json.Marshal(profile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content Type", "application/json")
+	w.Write(js)
+}
 func main() {
 
 	http.HandleFunc("/helloworld", hello)
+
+	http.HandleFunc("/versionz", version)
 
 	http.ListenAndServe(":8080", nil)
 }
